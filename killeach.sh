655b857cb614901fa40ac2b57a77e1f420ee059d
Cj-bc <cj-bc@y-modify.org>
2018-05-22 08:37:45 +0900

    Fix: killeach> detect whether stdin is exist. error handling.

--- killeach.sh
+++ killeach.sh
@@ -1,6 +1,9 @@
 #!/usr/local/bin/bash
 
-
+[ -p /dev/stdin ] || echo "please pass PID to kill from stdin." && exit 66
 while read pid; do
   kill $pid
+  [ $? -eq 0 ] || echo "couldn't kill $pid"
 done < <(cat -)
+
+exit 0
