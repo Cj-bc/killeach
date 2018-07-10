3f3e4d88a78abe9124aa2b6f833bfa6f05ea5b1c
Cj-bc <cj-bc@y-modify.org>
2018-07-10 11:35:59 +0900

    Add: support '-v' '-h' option

--- killeach.sh
+++ killeach.sh
@@ -7,6 +7,12 @@
 
 version=0.1.0
 
+case $1 in
+  "-h" ) echo "Kill all processed provided from stdin.";;
+  "-v" ) echo "killeach: version "$version;;
+esac
+
+
 [ -p /dev/stdin ] || echo "please pass PID to kill from stdin." && exit 66
 while read pid; do
   kill $pid
